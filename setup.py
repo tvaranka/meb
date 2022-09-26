5ad69027386184111a2c7adf72eec61b5038b9d0
tvaranka <tuomas.varanka@oulu.fi>
2022-09-26 15:16:12 +0300

    Updated setup file to include requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name="meb", version='1.0', packages=find_packages())
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name="meb", version='1.0', packages=find_packages(), install_requires=reqs)
