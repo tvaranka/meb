ab404b5a2b91bdc06981f4729bcc0eadd214333d
tvaranka <tuomas.varanka@oulu.fi>
2022-09-27 09:52:40 +0300

    Added optional requirements for traditional methods

--- meb/utils/__init__.py
+++ meb/utils/__init__.py
@@ -1,5 +1,4 @@
 from .py_evm import *
-from .traditional_methods import *
 from .utils import *
 from .mixup import *
 from .metrics import *
--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ def parse_requirements(filename):
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
-
-install_reqs = parse_requirements("requirements.txt")
-
-setup(name="meb", version='1.0', packages=find_packages(), install_requires=install_reqs)
+setup(
+    name="meb",
+    version='1.0',
+    packages=find_packages(),
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={
+        "optinal": parse_requirements("optional_requiremens.txt")
+    }
+)
