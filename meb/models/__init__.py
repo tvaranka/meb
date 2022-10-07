246af0ac8c4b0ca943633c18555b8c51e856ff9b
tvaranka <tuomas.varanka@oulu.fi>
2022-10-07 15:13:25 +0300

    Redid models init

--- meb/models/__init__.py
+++ meb/models/__init__.py
@@ -1,2 +1,22 @@
-from .me_networks import *
-from .RCNs import *
+from .me_networks import SSSNet
+from .me_networks import STSTNet
+from .me_networks import OffApexNet
+
+from .RCNs import RCN_A
+from .RCNs import RCN_C
+from .RCNs import RCN_W
+from .RCNs import RCN_F
+from .RCNs import RCN_P
+from .RCNs import RCN_S
+
+__all__ = [
+    "SSSNet",
+    "STSTNet",
+    "OffApexNet",
+    "RCN_A",
+    "RCN_C",
+    "RCN_W",
+    "RCN_F",
+    "RCN_P",
+    "RCN_S",
+]
