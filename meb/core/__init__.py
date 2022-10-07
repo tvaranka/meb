cdd56ba6100b22398c7392a4ebf1663053900647
tvaranka <tuomas.varanka@oulu.fi>
2022-10-07 14:57:33 +0300

    Redid core init

--- meb/core/__init__.py
+++ meb/core/__init__.py
@@ -1,1 +1,13 @@
-from .train_eval import *
+from .train_eval import Config
+from .train_eval import Validation
+from .train_eval import CrossDatasetValidation
+from .train_eval import IndividualDatasetAUValidation
+from .train_eval import MEGCValidation
+
+__all__ = [
+    "Config",
+    "Validation",
+    "CrossDatasetValidation",
+    "IndividualDatasetAUValidation",
+    "MEGCValidation",
+]
