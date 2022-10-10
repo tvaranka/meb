b3bb11499f8d18533e6b40e1f35b6531c0a386e3
tvaranka <tuomas.varanka@oulu.fi>
2022-10-10 08:30:18 +0300

    Redid datasets init

--- meb/datasets/__init__.py
+++ meb/datasets/__init__.py
@@ -1,3 +1,31 @@
-from .dataset_utils import *
-from .datasets import *
-from .sampling import *
+from .datasets import Smic
+from .datasets import Casme
+from .datasets import Casme2
+from .datasets import Casme3A
+from .datasets import Casme3C
+from .datasets import Casme3
+from .datasets import SAMM
+from .datasets import Fourd
+from .datasets import MMEW
+from .datasets import MEGC
+from .datasets import CrossDataset
+
+
+from .sampling import UniformTemporalSubsample
+from .sampling import NoisyUniformTemporalSubsample
+
+__all__ = [
+    "Smic",
+    "Casme",
+    "Casme2",
+    "Casme3A",
+    "Casme3C",
+    "Casme3",
+    "SAMM",
+    "Fourd",
+    "MMEW",
+    "MEGC",
+    "CrossDataset",
+    "UniformTemporalSubsample",
+    "NoisyUniformTemporalSubsample",
+]
