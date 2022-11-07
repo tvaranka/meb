0d852d931dd4136f0cadb5a469d0fd841d4f39e6
tvaranka <tuomas.varanka@oulu.fi>
2022-11-07 14:37:04 +0200

    Added test for dataloader

--- test/test_data/test_dataloader.py
+++ test/test_data/test_dataloader.py
@@ -1,24 +1,14 @@
-import unittest
-import pandas as pd
-import numpy as np
-from meb.datasets.dataset_utils import extract_action_units
+from .base import BaseTestDataset
 
 
-class TestDatasetUtils(unittest.TestCase):
-    def test_extract_action_units(self):
-        test_df = pd.DataFrame({"AU": ["AU5R", "AU4+1"]})
-        test_result_df = pd.DataFrame(
-            {
-                "AU": ["AU5R", "AU4+1"],
-                "AU1": [0, 1],
-                "AU4": [0, 1],
-                "AU5": [1, 0],
-            }
-        )
-        test_df_extracted = extract_action_units(test_df)
-        res = np.array(test_df_extracted == test_result_df).mean()
-        self.assertEqual(1, res)
-
-
-if __name__ == "__main__":
-    unittest.main()
+class TestCustomDataloader(BaseTestDataset):
+    def test_indexing(self):
+        c = self.per()
+        d = c.data
+        df = c.data_frame
+        assert len(d[0].shape) == 3
+        assert len(d[1:]) == 1
+        assert len(d[:-1]) == 1
+        assert len(d[[0, 1]]) == 2
+        idx = df["subject"] == "g1"
+        assert len(d[idx]) == 1
