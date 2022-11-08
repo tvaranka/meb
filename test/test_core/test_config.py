d62acb471fb39037b442027bb7e991045b6dbc63
tvaranka <tuomas.varanka@oulu.fi>
2022-11-08 13:42:45 +0200

    Add full test config

--- test/test_core/test_config.py
+++ test/test_core/test_config.py
@@ -3,15 +3,10 @@ from functools import partial
 import pytest
 import torch.nn as nn
 
-from meb.datasets.dataset_utils import LazyDataLoader, LoadedDataLoader
-from meb.core import Config  # , CrossDatasetValidator
+from meb.core import Config, CrossDatasetValidator
 from meb import utils, models
 from meb.utils.utils import ConfigException, validate_config
-
-# from ..test_data.base import BaseTestDataset
-
-a = LazyDataLoader
-b = LoadedDataLoader
+from ..test_data.base import BaseTestDataset
 
 
 def _copy_class_object(obj: object):
@@ -45,21 +40,21 @@ def test_incorrect_config():
         validate_config(cf)
 
 
-# def test_validators():
-#     cf = _copy_class_object(Config)
-#     cf.model = partial(models.SSSNet, num_classes=9)
-#     v = CrossDatasetValidator(cf)
-#     assert v.split_column == "dataset"
-#
-#
-# class TestValidator(BaseTestDataset):
-#     def test_validate_output(self):
-#         # Should be output list
-#         pass
-#
-#     def test_validate_print(self):
-#         # Test final printing output
-#         pass
-#
-#     def test_printer(self):
-#         pass
+def test_validators():
+    cf = _copy_class_object(Config)
+    cf.model = partial(models.SSSNet, num_classes=9)
+    v = CrossDatasetValidator(cf)
+    assert v.split_column == "dataset"
+
+
+class TestValidator(BaseTestDataset):
+    def test_validate_output(self):
+        # Should be output list
+        pass
+
+    def test_validate_print(self):
+        # Test final printing output
+        pass
+
+    def test_printer(self):
+        pass
