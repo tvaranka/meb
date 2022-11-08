5be34b59e62c4777e6575c2364b63a00478f9429
tvaranka <tuomas.varanka@oulu.fi>
2022-11-08 13:52:59 +0200

    Added base for test config

--- test/test_core/test_config.py
+++ test/test_core/test_config.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from meb.core import Config, CrossDatasetValidator
 from meb import utils, models
 from meb.utils.utils import ConfigException, validate_config
-from ..test_data.base import BaseTestDataset
+from .base import BaseTestDataset
 
 
 def _copy_class_object(obj: object):
