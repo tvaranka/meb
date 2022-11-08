4334684df3a125887b08dccec608b8ede59dbc7a
tvaranka <tuomas.varanka@oulu.fi>
2022-11-08 13:09:28 +0200

    Added partial test config

--- test/test_core/test_config.py
+++ test/test_core/test_config.py
@@ -1,45 +1,46 @@
-# from functools import partial
-#
-# import pytest
-# import torch.nn as nn
-#
-# from meb.core import Config, CrossDatasetValidator
-# from meb import utils, models
-# from meb.utils.utils import ConfigException, validate_config
+from functools import partial
+
+import pytest
+import torch.nn as nn
+
+from meb.core import Config  # , CrossDatasetValidator
+from meb import utils, models
+from meb.utils.utils import ConfigException, validate_config
+
 # from ..test_data.base import BaseTestDataset
-#
-#
-# def _copy_class_object(obj: object):
-#     """Copies class objects and their attributes."""
-#     return type("Object", (object,), dict(obj.__dict__))
-#
-#
-# def test_correct_config():
-#     cf = _copy_class_object(Config)
-#     cf.model = partial(models.SSSNet, num_classes=9)
-#     try:
-#         validate_config(cf)
-#     except ConfigException:
-#         pytest.fail("An exception was raised when it should not have been.")
-#
-#
-# def test_incorrect_config():
-#     cf = _copy_class_object(Config)
-#     cf.evaluation_fn = utils.MultiLabelF1Score()
-#     with pytest.raises(ConfigException):
-#         validate_config(cf)
-#
-#     cf = _copy_class_object(Config)
-#     cf.criterion = nn.CrossEntropyLoss()
-#     with pytest.raises(ConfigException):
-#         validate_config(cf)
-#
-#     cf = _copy_class_object(Config)
-#     cf.model = models.SSSNet(num_classes=9)
-#     with pytest.raises(ConfigException):
-#         validate_config(cf)
-#
-#
+
+
+def _copy_class_object(obj: object):
+    """Copies class objects and their attributes."""
+    return type("Object", (object,), dict(obj.__dict__))
+
+
+def test_correct_config():
+    cf = _copy_class_object(Config)
+    cf.model = partial(models.SSSNet, num_classes=9)
+    try:
+        validate_config(cf)
+    except ConfigException:
+        pytest.fail("An exception was raised when it should not have been.")
+
+
+def test_incorrect_config():
+    cf = _copy_class_object(Config)
+    cf.evaluation_fn = utils.MultiLabelF1Score()
+    with pytest.raises(ConfigException):
+        validate_config(cf)
+
+    cf = _copy_class_object(Config)
+    cf.criterion = nn.CrossEntropyLoss()
+    with pytest.raises(ConfigException):
+        validate_config(cf)
+
+    cf = _copy_class_object(Config)
+    cf.model = models.SSSNet(num_classes=9)
+    with pytest.raises(ConfigException):
+        validate_config(cf)
+
+
 # def test_validators():
 #     cf = _copy_class_object(Config)
 #     cf.model = partial(models.SSSNet, num_classes=9)
