74d27dab3816727b506bec708f9e2c5f6e51fd49
tvaranka <tuomas.varanka@oulu.fi>
2022-11-30 14:51:02 +0200

    Added tests for utils

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,6 +1,9 @@
+import copy
+
+import numpy as np
 import torch
 
-from meb import utils
+from meb import models, utils
 
 
 def test_mix_labels():
@@ -11,3 +14,22 @@ def test_mix_labels():
     assert torch.equal(out, torch.tensor([[0.0, 1.0], [1.0, 0.0]]))
     out = utils.mixup._mix_labels(labels, lam=1, label_smoothing=0.1)
     assert torch.equal(out, torch.tensor([[0.95, 0.05], [0.05, 0.95]]))
+
+
+def test_multi_label_f1_shape():
+    labels = torch.tensor([[1, 0], [0, 1]])
+    predictions = torch.tensor([[0.95, -0.4], [0.1, 0.6]])
+    metric1 = utils.MultiLabelF1Score(average="binary")
+    metric2 = utils.MultiLabelF1Score()
+    assert len(metric1(labels, predictions)) == 2
+    assert len(metric2(labels, predictions)) == 2
+
+
+def test_reset_weights():
+    m = models.SSSNet()
+    pc = copy.deepcopy(list(m.parameters()))
+    assert np.all([torch.equal(pc[i], list(m.parameters())[i]) for i in range(len(pc))])
+    utils.reset_weights(m)
+    assert np.all(
+        [~torch.equal(pc[i], list(m.parameters())[i]) for i in range(len(pc))]
+    )
